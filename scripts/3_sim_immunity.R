1bea3eda8cde2e1d1c5fd018d513a5bf97fd1705
Chu-Chang  Ku <lshck18@lshtm.ac.uk>
2024-01-15 10:22:40 +0000

    Expand coverage function

--- scripts/3_sim_immunity.R
+++ scripts/3_sim_immunity.R
@@ -7,30 +7,39 @@ source(here::here("R", "sim_coverage.R"))
 
 ## Inputs loading
 load(here::here("data", "fit_coverage.csv"))
+load(here::here("pars", "ves.rdata"))
 
-coverage <- read_csv(here::here("data", "Coverage.csv")) %>% 
-  mutate(
-    Cov_2018 = ifelse(Cov_2018 == "Not eligible", NA, Cov_2018),
-    Cov_2018 = as.numeric(Cov_2018)
-  ) %>% 
-  pivot_longer(-Age, 
-               names_pattern = "Cov_(\\d+)", names_to = "Year", values_to = "Coverage") %>% 
-  mutate(Coverage = Coverage / 100) 
-
-load(here::here("data", "VE_Zostavax_NIC_AJ.rdata"))
+ves <- ves %>% mutate(IC = ifelse(IC, "IC", "NonIC"))
 
 
-get_full_ve <- function(r_waning) {
-  VE$src %>% 
-    rename(AgeVac = age, VE0 = VE) %>% 
-    left_join(crossing(AgeVac = 0:100, FromVac = 0:100)) %>% 
+pop <- local({
+  load(here::here("data", "Population_ONS.rdata"))
+  load(here::here("data", "hz_burden_22.rdata"))
+  
+  pr_ic <- dat_burden %>% 
+    select(Age, IC = Immunocompromised, N) %>% 
+    pivot_wider(names_from = IC, names_prefix = "IC", values_from = N) %>% 
     mutate(
-      VE = VE0 * exp(- FromVac * r_waning),
-      Age = AgeVac + FromVac
+      PrIC = IC1 / IC0
     ) %>% 
-    filter(Age <= 100) %>% 
-    select(- VE0)
-}
+    select(Age, PrIC)
+
+  bind_rows(
+    demo_ons %>% 
+      filter(Location == "England") %>% 
+      mutate(IC = "NonIC") %>% 
+      select(Year, Age, N, IC),
+    demo_ons %>% 
+      filter(Location == "England") %>% 
+      left_join(pr_ic) %>% 
+      mutate(
+        N = round(N * PrIC),
+        N = ifelse(is.na(N), 0, N),
+        IC = "IC"
+      ) %>% 
+      select(Year, Age, N, IC)
+  )
+})
 
 
 ## Model of vaccine uptake
@@ -39,15 +48,136 @@ year0 <- 2014
 year1 <- 2040
 
 
-sims <- bind_rows(lapply(1944:2000, function(year_birth) {
+sims_Stay <- bind_rows(lapply(1944:2000, function(year_birth) {
   bind_rows(
-    simulate_protection(pars = pred1$pars, year_birth = year_birth, ic = F, year0 = year0, year1 = year1),
-    simulate_protection(pars = pred1$pars, year_birth = year_birth, ic = T, year0 = year0, year1 = year1)
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = F,
+                        al_ic = 70, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 70, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 70, ah_step2 = 80,
+                        year_step3 = 2033),
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = T,
+                        al_ic = 70, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 70, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 70, ah_step2 = 80,
+                        year_step3 = 2033)
   )
 })) %>% 
-  mutate(FromVac = Year - YearVac)
+  mutate(FromVac = Year - YearVac, Scenario = "Stay") %>% 
+  left_join(pop, relationship = "many-to-many") %>% 
+  left_join(ves, relationship = "many-to-many") %>% 
+  filter(!is.na(VE))
 
-save(sims, file = here::here("outputs", "temp", "sims_coverage.rdata"))
+
+sims_Plan <- bind_rows(lapply(1944:2000, function(year_birth) {
+  bind_rows(
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = F,
+                        al_ic = 50, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 65, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 60, ah_step2 = 80,
+                        year_step3 = 2033),
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = T,
+                        al_ic = 50, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 65, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 60, ah_step2 = 80,
+                        year_step3 = 2033)
+  )
+})) %>% 
+  mutate(FromVac = Year - YearVac, Scenario = "UK_Plan") %>% 
+  left_join(pop, relationship = "many-to-many") %>% 
+  left_join(ves, relationship = "many-to-many") %>% 
+  filter(!is.na(VE))
+
+
+sims_IcOnly <- bind_rows(lapply(1944:2000, function(year_birth) {
+  bind_rows(
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = F,
+                        al_ic = 50, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 70, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 70, ah_step2 = 80,
+                        year_step3 = 2033),
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = T,
+                        al_ic = 50, ah_ic = 80,
+                        year_step1 = 2023, al_step1 = 70, ah_step1 = 80,
+                        year_step2 = 2028, al_step2 = 70, ah_step2 = 80,
+                        year_step3 = 2033)
+  )
+})) %>% 
+  mutate(FromVac = Year - YearVac, Scenario = "IC_Only") %>% 
+  left_join(pop, relationship = "many-to-many") %>% 
+  left_join(ves, relationship = "many-to-many") %>% 
+  filter(!is.na(VE))
+
+
+
+sims_PlanAndOlder <- bind_rows(lapply(1944:2000, function(year_birth) {
+  bind_rows(
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = F,
+                        al_ic = 50, ah_ic = 90,
+                        year_step1 = 2023, al_step1 = 65, ah_step1 = 90,
+                        year_step2 = 2028, al_step2 = 60, ah_step2 = 90,
+                        year_step3 = 2033),
+    simulate_protection(pred1$pars$p_initial, pred1$pars$p_catchup, year_birth = year_birth, 
+                        year0 = year0, year1 = year1, ic = T,
+                        al_ic = 50, ah_ic = 90,
+                        year_step1 = 2023, al_step1 = 65, ah_step1 = 90,
+                        year_step2 = 2028, al_step2 = 60, ah_step2 = 90,
+                        year_step3 = 2033)
+  )
+})) %>% 
+  mutate(FromVac = Year - YearVac, Scenario = "UK_Plan to 90") %>% 
+  left_join(pop, relationship = "many-to-many") %>% 
+  left_join(ves, relationship = "many-to-many") %>% 
+  filter(!is.na(VE))
+
+
+
+save(sims_Stay, sims_Plan, sims_IcOnly, sims_PlanAndOlder, file = here::here("outputs", "temp", "sims_coverage.rdata"))
+
+# save(sims, file = here::here("outputs", "temp", "sims_coverage.rdata"))
+
+
+
+
+
+
+vis <- function(sims) {
+  list(
+    Coverage= sims %>% 
+      filter(Year %in% c(2018, 2023, 2028, 2033)) %>% 
+      ggplot() +
+      geom_bar(aes(x = Age, y = Pr, fill = FromVac), stat = "identity", position = "stack", width = 1) +
+      scale_y_continuous("Coverage, %", labels = scales::percent) +
+      scale_fill_gradient("Year from vaccined", high = "grey80", low = "grey40", breaks = seq(0, 20, 5)) +
+      expand_limits(y = 1, x = 50) +
+      facet_grid(Year~IC) +
+      theme(axis.text.x = element_text(angle = -60, hjust = 0, vjust = 0)),
+    VE = sims %>% 
+      filter(Year %in% c(2018, 2023, 2028, 2033)) %>% 
+      ggplot() +
+      geom_bar(aes(x = Age, y = Pr * VE, fill = FromVac), stat = "identity", position = "stack", width = 1) +
+      scale_y_continuous("Protection, %", labels = scales::percent) +
+      scale_fill_gradient("Year from vaccined", high = "grey80", low = "grey40", breaks = seq(0, 20, 5)) +
+      expand_limits(y = 1, x = 50) +
+      facet_grid(Year~IC) +
+      theme(axis.text.x = element_text(angle = -60, hjust = 0, vjust = 0)) 
+  )
+}
+
+
+vis_Stay = vis(sims_Stay)
+vis_Plan = vis(sims_Plan)
+vis_IcOnly = vis(sims_IcOnly)
+vis_PlanAndOlder = vis(sims_PlanAndOlder)
+
+  
+  
 
 
 ### Visualisation
