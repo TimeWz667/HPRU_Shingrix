087988b459fa25006ec638788a1c44a76dae7f6d
TimeWz667 <TimeWz667@gmail.com>
2024-01-22 23:10:58 +0000

    Compile PtIC

--- scripts/data/0_compile_demography_pic.R
+++ scripts/data/0_compile_demography_pic.R
@@ -1,5 +1,20 @@
 library(tidyverse)
+library(readxl)
 
 
+raw <- read_xlsx(here::here("data", "raw", "Zoster_Burden_Results_Nov19_Trimmed.xlsx"), sheet = 1)
 
+
+PrIC <- raw %>% 
+  select(Age, IC = Immunocompromised, N = `Person- years`) %>% 
+  pivot_wider(names_from = IC, values_from = N, names_prefix = "IC_") %>% 
+  mutate(
+    PrIC = IC_1 / (IC_0 + IC_1) 
+  ) %>% 
+  select(Age, PrIC)
+
+
+PrIC 
+
+save(PrIC, file = here::here("data", "processed_demography", "PrIC_GDPR.rdata"))
 
