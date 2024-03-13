20b60aa3b5fd076729767d340e67c51ee671f367
TimeWz667 <TimeWz667@gmail.com>
2024-03-13 01:25:25 +0000

    Plan validation

---
+++ scripts/6_validate_with_previous_version.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+theme_set(theme_bw())
+
+
+### 
+# Age: 70 and 75
+# IC: Non-IC
+###
+
+
+## AC's version
+
+
+## Fix QOL loss due to HZ-related death
+
+
+## Move to cohort-based simulation
+
+
+## Move to 2023 population and background mortality
+
+
+## Make time going forward
