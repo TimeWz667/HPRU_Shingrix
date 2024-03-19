715cce6b94c95bf7e555fc2bfbd85f918adb9f6c
Chu-ChangKu <timewz667@gmail.com>
2024-03-19 13:30:02 +0000

    Model ZVL ve

--- scripts/3_3_fit_ve_zvl_zi_stan.R
+++ scripts/3_3_fit_ve_zvl_zi_stan.R
@@ -5,7 +5,7 @@ library(readxl)
 theme_set(theme_bw())
 
 
-options(mc.cores = 6)
+options(mc.cores = 4)
 rstan_options(auto_write = TRUE)
 
 
@@ -16,6 +16,8 @@ model <- stan_model(here::here("models", "coxian_gamma.stan"))
 dat_ve <- read_xlsx(here::here("data", "VE.xlsx"), sheet = 1) %>% 
   filter(Use) %>% 
   filter(Type == "HZ") %>% 
+  filter(Vaccine == "Zostavax") %>% 
+  filter(Source == "Klein 2023") %>% 
   mutate(
     M = M / 100,
     L = L / 100, 
@@ -36,16 +38,14 @@ dat_ve %>%
 
 
 ds <- dat_ve %>% 
-  filter(Source == "Strezova 2023") %>% 
+  filter(Follow_up == 1) %>% 
   mutate(
-    # sd = (U - L) / 2 / 1.96,
-    # n = M * (1 - M) / sd ** 2,
-    # n = round(n),
-    n = c(14035, 13564, 13074, 12517, 7277, 7100, 6878, 6648, 6258),
+    sd = (U - L) / 2 / 1.96,
+    n = M * (1 - M) / sd ** 2,
+    n = round(n),
     y = round(n * M)
   ) %>% 
   select(n, yr = Yr, y) %>% 
-  filter(yr > 1) %>% 
   as.list()
 
 ds$N <- length(ds$yr)
@@ -55,7 +55,7 @@ post <- sampling(model, data = ds, iter = 5000)
 
 post
 
-save(post, file = here::here("outputs", "temp", "pars_ve_rzv_zlgamma.rdata"))
+save(post, file = here::here("outputs", "temp", "pars_ve_zvl_zlgamma.rdata"))
 
 
 sel <- data.frame(rstan::extract(post, pars = c("p0", "alpha", "beta"))) %>% 
@@ -65,7 +65,8 @@ sel <- data.frame(rstan::extract(post, pars = c("p0", "alpha", "beta"))) %>%
 
 # sel %>% 
 #   filter(Key <= 3000) %>% 
-#   write_csv(here::here("pars", "pars_ve_rzv_zlgamma.csv"))
+#   write_csv(here::here("pars", "pars_ve_zvl_zlgamma.csv"))
+
 
 
 sims <- sel %>% 
@@ -73,9 +74,9 @@ sims <- sel %>%
   full_join(crossing(Key = 1:1000, Yr = 1:30)) %>% 
   mutate(
     VE = p0 * (1 - pgamma(Yr, alpha, beta))
-  )  %>% 
+  ) %>% 
   select(Key, Yr, VE) %>% 
-  write_csv(here::here("pars", "sims_ve_rzv_zlgamma.csv"))
+  write_csv(here::here("pars", "sims_ve_zvl_zlgamma.csv"))
 
 
 g_gof <- sims %>% 
@@ -88,14 +89,14 @@ g_gof <- sims %>%
   ggplot() +
   geom_ribbon(aes(x = Yr, ymin = L, ymax = U), alpha = 0.2) +
   geom_line(aes(x = Yr, y = M)) +
-  geom_pointrange(data = dat_ve %>% filter(!Realworld), aes(x = Yr, y = M, ymin = L, ymax = U)) +
+  geom_pointrange(data = dat_ve %>% filter(Follow_up == 1), aes(x = Yr, y = M, ymin = L, ymax = U)) +
   scale_y_continuous("Vaccine efficacy, %", label = scales::percent) +
   scale_x_continuous("Year since vaccinated") +
-  expand_limits(y = 0)
+  expand_limits(y = 0:1)
 
 g_gof
 
-ggsave(g_gof, filename = here::here("outputs", "figs", "g_pars_ve_rzv_zlgamma.png"), width = 7, height = 5.5)
+ggsave(g_gof, filename = here::here("outputs", "figs", "g_pars_ve_zvl_zlgamma.png"), width = 7, height = 5.5)
 
 
 
