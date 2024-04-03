adddb1f34546b0e18621a2d874b9dd40f2847841
TimeWz667 <TimeWz667@gmail.com>
2024-04-03 09:28:19 +0100

    Update figure

--- analysis_cohort/5_vis_revac.R
+++ analysis_cohort/5_vis_revac.R
@@ -1,18 +1,37 @@
 library(tidyverse)
 
+theme_set(theme_bw())
+
 
 load(here::here("analysis_cohort", "temp", "stats_rzv2rzv.rdata"))
 
+
+g_icer0 <- stats_ce %>% 
+  filter(Age1 == Age0 + 1) %>% 
+  filter(Arm == "Vac") %>% 
+  filter(Index == "ICER") %>% 
+  ggplot() +
+  geom_ribbon(aes(x = Age0, ymin = L, ymax = U), alpha = 0.2) +
+  geom_smooth(aes(x = Age0, y = Avg), se = F) +
+  geom_hline(yintercept = 2e4, linetype = 2) +
+  geom_hline(yintercept = 3e4, linetype = 2) +
+  scale_x_continuous("Age for RZV vaccination") +
+  scale_y_continuous("ICER") +
+  expand_limits(y = 0)
+
+
 g_icer <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "ICER") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
   geom_vline(aes(xintercept = Age0), linetype = 2) +
   geom_text(aes(x = Age0, y = 0), label = "First RZV", angle = 90, hjust = 0, vjust = 1.2) +
+  geom_hline(yintercept = 2e4, linetype = 2) +
+  geom_hline(yintercept = 3e4, linetype = 2) +
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("ICER") +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_icer
@@ -20,7 +39,7 @@ g_icer
 
 
 g_e <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "dQ_All_d") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
@@ -28,14 +47,14 @@ g_e <- stats_ce %>%
   geom_text(aes(x = Age0, y = 0), label = "First RZV", angle = 90, hjust = 0, vjust = 1.2) +
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("QALY gained") +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_e
 
 
 g_c <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "dC_All_d") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
@@ -44,12 +63,12 @@ g_c <- stats_ce %>%
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("Incremental cost, million GBP", 
                      labels = scales::number_format(scale = 1e-6)) +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_c
 
-
+ggsave(g_icer0, filename = here::here("analysis_cohort", "figs", "g_rzv_icer.png"), width = 5, height = 3.5)
 ggsave(g_e, filename = here::here("analysis_cohort", "figs", "g_rzv2rzv_de.png"), width = 8, height = 5)
 ggsave(g_c, filename = here::here("analysis_cohort", "figs", "g_rzv2rzv_dc.png"), width = 8, height = 5)
 ggsave(g_icer, filename = here::here("analysis_cohort", "figs", "g_rzv2rzv_icer.png"), width = 8, height = 5)
@@ -58,16 +77,33 @@ ggsave(g_icer, filename = here::here("analysis_cohort", "figs", "g_rzv2rzv_icer.
 
 load(here::here("analysis_cohort", "temp", "stats_zvl2rzv.rdata"))
 
+
+g_icer0 <- stats_ce %>% 
+  filter(Age1 == Age0 + 1) %>% 
+  filter(Arm == "Vac") %>% 
+  filter(Index == "ICER") %>% 
+  ggplot() +
+  geom_ribbon(aes(x = Age0, ymin = L, ymax = U), alpha = 0.2) +
+  geom_smooth(aes(x = Age0, y = Avg), se = F) +
+  geom_hline(yintercept = 2e4, linetype = 2) +
+  geom_hline(yintercept = 3e4, linetype = 2) +
+  scale_x_continuous("Age for ZVL vaccination") +
+  scale_y_continuous("ICER") +
+  expand_limits(y = 0)
+
+
 g_icer <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "ICER") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
   geom_vline(aes(xintercept = Age0), linetype = 2) +
   geom_text(aes(x = Age0, y = 0), label = "First RZV", angle = 90, hjust = 0, vjust = 1.2) +
+  geom_hline(yintercept = 2e4, linetype = 2) +
+  geom_hline(yintercept = 3e4, linetype = 2) +
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("ICER") +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_icer
@@ -75,7 +111,7 @@ g_icer
 
 
 g_e <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "dQ_All_d") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
@@ -83,14 +119,14 @@ g_e <- stats_ce %>%
   geom_text(aes(x = Age0, y = 0), label = "First RZV", angle = 90, hjust = 0, vjust = 1.2) +
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("QALY gained") +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_e
 
 
 g_c <- stats_ce %>% 
-  filter(Age0 %in% c(60, 65, 70, 75, 80)) %>% 
+  filter(Age0 %in% c(60, 65, 70, 75, 80, 85, 90)) %>% 
   filter(Index == "dC_All_d") %>% 
   ggplot() +
   geom_line(aes(x = Age1, y = Avg, colour = Arm)) +
@@ -99,12 +135,13 @@ g_c <- stats_ce %>%
   scale_x_continuous("Age for RZV revaccination") +
   scale_y_continuous("Incremental cost, million GBP", 
                      labels = scales::number_format(scale = 1e-6)) +
-  facet_grid(.~Age0) +
+  facet_wrap(.~Age0) +
   expand_limits(y = 0)
 
 g_c
 
 
+ggsave(g_icer0, filename = here::here("analysis_cohort", "figs", "g_zvl_icer.png"), width = 5, height = 3.5)
 ggsave(g_e, filename = here::here("analysis_cohort", "figs", "g_zvl2rzv_de.png"), width = 8, height = 5)
 ggsave(g_c, filename = here::here("analysis_cohort", "figs", "g_zvl2rzv_dc.png"), width = 8, height = 5)
 ggsave(g_icer, filename = here::here("analysis_cohort", "figs", "g_zvl2rzv_icer.png"), width = 8, height = 5)
