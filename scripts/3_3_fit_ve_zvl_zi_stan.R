302e4df521d5dc1f7abef07e622a1bab125fc1ae
TimeWz667 <TimeWz667@gmail.com>
2024-05-15 00:28:03 +0100

    Add zl gamma and exp for zvl

--- scripts/3_3_fit_ve_zvl_zi_stan.R
+++ scripts/3_3_fit_ve_zvl_zi_stan.R
@@ -5,12 +5,8 @@ library(readxl)
 theme_set(theme_bw())
 
 
-options(mc.cores = 4)
+options(mc.cores = 6)
 rstan_options(auto_write = TRUE)
-
-
-model <- stan_model(here::here("models", "coxian_gamma.stan"))
-
 
 
 dat_ve <- read_xlsx(here::here("data", "VE.xlsx"), sheet = 1) %>% 
@@ -72,7 +68,7 @@ ds <- dat_ve %>%
   mutate(
     sd = (U - L) / 2 / 1.96,
     n = M * (1 - M) / sd ** 2,
-    n = round(n),
+    n = round(n / n()),
     y = round(n * M)
   ) %>% 
   select(n, yr = Yr, y) %>% 
@@ -81,24 +77,54 @@ ds <- dat_ve %>%
 ds$N <- length(ds$yr)
 
 
-post <- sampling(model, data = ds, iter = 5000)
+for (key_model in c("zl_exp", "zl_gamma")) {
+  model <-  stan_model(here::here("models", key_model + glue::as_glue(".stan")))
+  
+  post <- sampling(model, data = ds, chains = 3, iter = 2000, warmup = floor(2000 - 1000))
+  
+  
+  if (key_model == "zl_gamma") {
+    sel <- data.frame(rstan::extract(post, pars = c("p0", "alpha", "beta"))) %>%
+      as_tibble()
+  } else if (key_model == "zl_exp") {
+    sel <- data.frame(rstan::extract(post, pars = c("p0", "beta"))) %>%
+      as_tibble() %>%
+      mutate(alpha = 1)
+  }
+  
+  sel <- sel %>% mutate(Key = 1:n())
+  
+  sims <- sel %>%
+    filter(Key <= 1000) %>%
+    full_join(crossing(Key = 1:1000, Yr = 1:30)) %>%
+    mutate(
+      VE = p0 * (1 - pgamma(Yr, alpha, beta))
+    )  %>%
+    select(Key, Yr, VE)
+  
+  
+  g_gof <- sims %>% 
+    group_by(Yr) %>% 
+    summarise(
+      M = mean(VE),
+      L = quantile(VE, 0.025),
+      U = quantile(VE, 0.975)
+    ) %>% 
+    ggplot() +
+    geom_ribbon(aes(x = Yr, ymin = L, ymax = U), alpha = 0.2) +
+    geom_line(aes(x = Yr, y = M)) +
+    geom_pointrange(data = dat_ve, aes(x = Yr, y = M, ymin = L, ymax = U)) +
+    scale_y_continuous("Vaccine efficacy, %", label = scales::percent) +
+    scale_x_continuous("Year since vaccinated") +
+    expand_limits(y = 0)
+  
+  
+  save(post, file = here::here("outputs", "temp", "pars_ve_zvl_" + glue::as_glue(key_model) + ".rdata"))
+  write_csv(sims, here::here("pars", "sims_ve_zvl_" + glue::as_glue(key_model) + ".csv"))
+  ggsave(g_gof, filename = here::here("outputs", "figs", "g_pars_ve_zvl_" + glue::as_glue(key_model) + ".png"), width = 7, height = 5.5)
+  
+}
 
-post
-
-save(post, file = here::here("outputs", "temp", "pars_ve_zvl_zlgamma.rdata"))
-
-
-dat_ve
-
-
-sel <- data.frame(rstan::extract(post, pars = c("p0", "alpha", "beta"))) %>% 
-  as_tibble() %>% 
-  mutate(Key = 1:n())
-
-
-sel %>%
-  filter(Key <= 3000) %>%
-  write_csv(here::here("pars", "pars_ve_zvl_zlgamma.csv"))
 
 
 d_agp <- tibble(Age = 50:100) %>% 
@@ -139,36 +165,5 @@ g_test_agp <- d_agp %>%
   geom_line(aes(x = Age, y = M4)) +
   expand_limits(y = 0:1)
 
-
-
-sims <- sel %>% 
-  filter(Key <= 1000) %>% 
-  full_join(crossing(Key = 1:1000, Yr = 1:30)) %>% 
-  mutate(
-    VE = p0 * (1 - pgamma(Yr, alpha, beta))
-  ) %>% 
-  select(Key, Yr, VE) %>% 
-  write_csv(here::here("pars", "sims_ve_zvl_zlgamma.csv"))
-
-
-g_gof <- sims %>% 
-  group_by(Yr) %>% 
-  summarise(
-    M = mean(VE),
-    L = quantile(VE, 0.025),
-    U = quantile(VE, 0.975)
-  ) %>% 
-  ggplot() +
-  geom_ribbon(aes(x = Yr, ymin = L, ymax = U), alpha = 0.2) +
-  geom_line(aes(x = Yr, y = M)) +
-  geom_pointrange(data = dat_ve %>% filter(Follow_up == 1), aes(x = Yr, y = M, ymin = L, ymax = U)) +
-  scale_y_continuous("Vaccine efficacy, %", label = scales::percent) +
-  scale_x_continuous("Year since vaccinated") +
-  expand_limits(y = 0:1)
-
-g_gof
-
-ggsave(g_gof, filename = here::here("outputs", "figs", "g_pars_ve_zvl_zlgamma.png"), width = 7, height = 5.5)
-
-
+g_test_agp
 
