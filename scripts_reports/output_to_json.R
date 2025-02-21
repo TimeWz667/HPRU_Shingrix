491cda09adb36c8e431aa2fc1503121637ee752e
Chu-Chang Ku <timewz667@gmail.com>
2025-02-21 20:29:16 +0000

    Output results for app

---
+++ scripts_reports/output_to_json.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(jsonlite)
+
+
+ds <- dir("docs/tabs")
+ds <- ds[endsWith(ds, "_ce.csv") | endsWith(ds, "_ys.csv")]
+
+
+for (file in ds[endsWith(ds, "_ce.csv")]) {
+  tab <- read_csv(here::here("docs", "tabs", file))
+
+  if (startsWith(file, "tab_re")) {
+    tab %>% 
+      select(-c(A, L, U)) %>% 
+      group_by(Scenario, Arm, Age0, Age1) %>% 
+      pivot_wider(names_from = Index, values_from = M) %>% 
+      write_json(here::here("json", gsub(".csv", ".json", file)))
+  } else {
+    tab %>% 
+      select(-c(A, L, U)) %>% 
+      group_by(Scenario, Arm, Age0) %>% 
+      pivot_wider(names_from = Index, values_from = M) %>% 
+      write_json(here::here("json", gsub(".csv", ".json", file)))
+  }
+}
