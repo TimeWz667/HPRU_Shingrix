4dd23a52820fa65a7e412286cccd39041931e5d3
Chu-Chang Ku <timewz667@gmail.com>
2025-04-15 17:12:56 +0100

    Update json outputs

--- scripts_reports/output_to_json.R
+++ scripts_reports/output_to_json.R
@@ -2,44 +2,36 @@ library(tidyverse)
 library(jsonlite)
 
 
-ds <- dir("docs/tabs")
-ds <- ds[endsWith(ds, "_ce.csv") | endsWith(ds, "_ys.csv")]
-
-
-for (file in ds[endsWith(ds, "_ce.csv")]) {
-  tab <- read_csv(here::here("docs", "tabs", file))
-
-  if (startsWith(file, "tab_re")) {
-    tab %>% 
-      select(-c(A, L, U)) %>% 
-      group_by(Scenario, Arm, Age0, Age1) %>% 
-      pivot_wider(names_from = Index, values_from = M) %>% 
-      write_json(here::here("json", gsub(".csv", ".json", file)))
-  } else {
-    tab %>% 
+for (vtype in c("rw", "tr")) {
+  for (dis in c("0", "15", "35")) {
+    folder <- glue::as_glue(vtype) + "_" + dis
+    
+    read_csv(here::here("docs", "tabs", folder, "stats_uv_ce.csv")) %>% 
       select(-c(A, L, U)) %>% 
       group_by(Scenario, Arm, Age0) %>% 
       pivot_wider(names_from = Index, values_from = M) %>% 
-      write_json(here::here("json", gsub(".csv", ".json", file)))
-  }
-}
-
-
-for (file in ds[endsWith(ds, "_ys.csv")]) {
-  tab <- read_csv(here::here("docs", "tabs", file))
-  
-  if (startsWith(file, "tab_re")) {
-    tab %>% 
+      write_json(here::here("json", "stats_ce_uv_" + folder + "json"))
+    
+    read_csv(here::here("docs", "tabs", folder, "stats_re_ce.csv")) %>% 
       select(-c(A, L, U)) %>% 
       group_by(Scenario, Arm, Age0, Age1) %>% 
-      pivot_wider(names_from = Index, values_from = M) %>% 
-      write_json(here::here("json", gsub(".csv", ".json", file)))
-  } else {
-    tab %>% 
+      pivot_wider(names_from = Index, values_from = M) %>%
+      write_json(here::here("json", "stats_ce_re_" + folder + "json"))
+    
+    
+    read_csv(here::here("docs", "tabs", folder, "stats_uv_ys.csv")) %>% 
       select(-c(A, L, U)) %>% 
       group_by(Scenario, Arm, Age0) %>% 
       pivot_wider(names_from = Index, values_from = M) %>% 
-      write_json(here::here("json", gsub(".csv", ".json", file)))
+      write_json(here::here("json", "stats_ys_uv_" + folder + "json"))
+    
+    read_csv(here::here("docs", "tabs", folder, "stats_re_ys.csv")) %>% 
+      select(-c(A, L, U)) %>% 
+      group_by(Scenario, Arm, Age0, Age1) %>% 
+      pivot_wider(names_from = Index, values_from = M) %>% 
+      write_json(here::here("json", "stats_ys_re_" + folder + "json"))
+    
+    
   }
 }
 
